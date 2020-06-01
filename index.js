34c2d2a4656d548680ca2b32eb8fc435e3e36c7e
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-06-01 14:27:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,50 @@ let loaf = {
     water: 210,
 
     hydration: function () {
-        console.log(this.water / this.flour * 100);
+        return (this.water / this.flour * 100);
     },
 
 };
 
+console.log(loaf.flour);
+
+let object1 = {
+    foo: 1,
+    bar: "Hello",
+    fum:2,
+    quux:"World",
+    spam:"!"
+};
+
+for (let property in object1) {
+    console.log(property + ": " + object1[property]);
+}
+
+let object2 = {
+    meals: ['breakfast', 'second breakfast', 'elevenses',
+        'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(object2.meals[3]);
+
+let object3 = [
+    {
+        name: "Abdu",
+        jobTitle: "Engineer",
+    },
+
+    {
+        name: "Seyi",
+        jobTitle: "Pair"
+    },
+
+    {
+        name: "Jenna",
+        jobTitle: "Intern"
+    },
+];
+
+
+object3.forEach(element => {
+    console.log(element);
+});
